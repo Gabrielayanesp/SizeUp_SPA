ce21751156e90830e7769bd185826d0bf20d9438
gabrielayanesp <gabriela.yanesp17@gmail.com>
2025-08-30 18:59:16 -0500

    Update, new web sizeup changes

--- js/seed.js
+++
@@ -1,8 +0,0 @@
-let users = JSON.parse(localStorage.getItem("users")) || [];
-users.push({ 
-  name: "Super Admin", 
-  email: "admin@onheels.com", 
-  password: "123456", 
-  role: "admin" 
-});
-localStorage.setItem("users", JSON.stringify(users));
